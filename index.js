1928c4d123ec76e60156f9737c031cd6fee1a090
Anders Jurisoo <jurisoo@hotmail.com>
2020-02-02 17:53:59 +0100

    Testing merging

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const fs = require('fs');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  core.setOutput("current_dirname", __dirname);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    let data1 = JSON.parse(
+        fs.readFileSync('data1.json')
+    );
+    let data2 = JSON.parse(
+        fs.readFileSync('data2.json')
+    );
+
+    let data3 = {
+        ...data1,
+        ...data2
+    }
+    
+    core.setOutput("data3", data3);
+
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
+
+/* OLD
+    core.setOutput("time", time);
+    const github = require('@actions/github');
+    //const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+*/
