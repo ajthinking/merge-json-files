3ee83e70fcb9bb1f4d8a549973a5f4f507e27928
Anders Jurisoo <jurisoo@hotmail.com>
2020-02-05 11:23:13 +0100

    test

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const core = require('@actions/core');
 const fs = require('fs');
 
 try {
-    let root = JSON.parse(core.getInput('runner_context')).workspace  + '/merge-json-files';
-
-    console.log("My root is " + root);
+    let workspace = JSON.parse(core.getInput('runner_context')).workspace;
+    let root = `${workspace}/${workspace.split("/").pop()}`
 
     fs.readdirSync(root, function (err, files) {
         //handling error
